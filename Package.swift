08c7b080ae9fa930fe9f2772bef24241160a86c3
Brandon Wong Ka Seng <gouen1995@gmail.com>
2020-10-23 18:38:49 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/CocoaLumberjack/CocoaLumberjack.git", .upToNextMajor(from: "3.7.0"))
+        .package(url: "https://github.com/gouen95/CocoaLumberjack.git", .upToNextMajor(from: "3.7.0"))
     ],
     targets: [
         .target(
